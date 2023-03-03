4ae4dd7a5fc558a21c1fafb83cf1836cfe92e968
TeamPlayer99 <32104436+TeamPlayer99@users.noreply.github.com>
2023-03-04 01:19:31 +0200

    modified Open close

---
+++ OpenCloseApp/OpenCloseApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenCloseApp.Lib;
+
+namespace OpenCloseApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+
+            Console.WriteLine(shop.Open());
+            Console.WriteLine(shop.Start());
+            Console.WriteLine(shop.Stop());
+            Console.WriteLine(shop.Close());
+        }
+    }
+}
